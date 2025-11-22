df9477f8202e1107d874be8c3957d44314a92931
cbroko <17567294+cbroko@users.noreply.github.com>
2025-11-22 15:15:53 +0100

    torque

--- game-jam-full-circle/frog.gd
+++ game-jam-full-circle/frog.gd
@@ -1,6 +1,9 @@
 extends RigidBody3D
 
 @export var move_force: float = 20.0
+@export var torque_strength: float = 5.0
+@export var max_angular_speed: float = 10.0
+
 
 func _physics_process(delta: float) -> void:
 	var input_vector = Vector2.ZERO
@@ -23,8 +26,19 @@ func _physics_process(delta: float) -> void:
 	direction.z = input_vector.y
 	
 	if(direction != Vector3.ZERO):
-		apply_central_force(direction * move_force)
+		#apply_central_force(direction * move_force)
+		var torque: Vector3 = direction.cross(Vector3.UP) * torque_strength
+		apply_torque(torque)
 	
+	
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	var ang_vel = state.angular_velocity
+	var ang_speed = ang_vel.length()
+	
+	if(ang_speed > max_angular_speed):
+		ang_vel = ang_vel.normalized() * max_angular_speed
+		state.angular_velocity = ang_vel 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
